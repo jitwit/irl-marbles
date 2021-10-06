546814b738079af08bb2cbf946cef4a3ed3e2cdc
Joseph Novakovich <josephnovakovich@gmail.com>
2021-10-06 13:48:54 -0400

    minorly

--- main.rkt
+++ main.rkt
@@ -6,40 +6,41 @@
          
          "irc.rkt")
 
-(define pieces->names
-  '((p "black pawn")
-    (n "black knight")
-    (b "black bishop")
-    (r "black rook")
-    (q "black queen")
-    (k "black king")
-    (P "white pawn")
-    (N "white knight")
-    (B "white bishop")
-    (R "whtie rook")
-    (Q "white queen")
-    (K "white king")))
+(define pieces.names
+  '((p . "black pawn")
+    (n . "black knight")
+    (b . "black bishop")
+    (r . "black rook")
+    (q . "black queen")
+    (k . "black king")
+    (P . "white pawn")
+    (N . "white knight")
+    (B . "white bishop")
+    (R . "whtie rook")
+    (Q . "white queen")
+    (K . "white king")))
 
 (define pieces
-  (map car pieces->names))
+  (map car pieces.names))
 
 (define (piece->name piece)
-  (let ((name (assq piece pieces->names)))
-    (and name (cadr name))))
+  (let ((name (assq piece pieces.names)))
+    (and name (cdr name))))
 
 (define participants
   (make-hash))
 
 (define (available-pieces)
   (filter (lambda (p)
-            (not (hash-ref participants p #f)))
+            (not (lookup-piece p)))
           pieces))
 
 (define add-messages
   '(ok already-assigned piece-taken marbles-full))
 
 (define (add-participant who piece)
-  (cond ((member who (hash-values participants)) 'already-assigned)
+  (cond ((member who (hash-values participants))
+         'already-assigned)
         ((not piece) ;; was #f from randomly getting a piece
          'marbles-full)
         ((not (lookup-piece piece))
@@ -111,133 +112,109 @@
     "reset"
     "commands"))
 
-(define (respond-to-message message where what)
-  (let ((who (cdr (assq 'display-name (irc-message-tags message)))))
-    (and who
-         (match (string-split what)
-           ('("?play")
-            ;; todo mutex or something 
-            (let* ((piece (random-piece))
-                   (result (add-participant who (random-piece))))
-              (match result
-                ('ok
-                 (irc-send-message C
-                                   where
-                                   (format "@~a you have the ~a"
-                                           who
-                                           (piece->name piece))))
-                ('already-assigned
-                 (irc-send-message C
-                                   where
-                                   (format "@~a you already have a piece"
-                                           who)))
-                ('marbles-full
-                 (irc-send-message C
-                                   where
-                                   (format "@~a marbbies is full"
-                                           who)))))
-            (write participants) (newline))
-           ('("?leave")
-            (remove-participant who)
-            (irc-send-message C
-                              where
-                              (format "@~a left the game"
-                                      who))
-            (write participants) (newline))
-           ('("?what")
-            (let ((piece (participant-piece who)))
-              (irc-send-message C
-                                where
-                                (if piece
-                                    (format "@~a you have the ~a"
-                                            who
-                                            (and piece
-                                                 (piece->name piece)))
-                                    (format "@~a you are not in the irl marbbies"
-                                            who)))
-              (write participants)) (newline))
-           (`("?who" ,piece)
-            (let* ((piece (string->symbol piece))
-                   (pig (lookup-piece piece)))
-              (write (list pig piece)) (newline)
-              (cond ((not (member piece pieces))
-                     (irc-send-message C
-                                       where
-                                       (format "@~a, ~a is not a piece. expecting one of: ~a"
-                                               who
-                                               piece
-                                               (string-join
-                                                (map symbol->string pieces)
-                                                ", "))))
-                    (else
-                     (irc-send-message C
-                                       where
-                                       (if pig
-                                           (format "@~a, ~a has the ~a"
-                                                   who
-                                                   pig
-                                                   (piece->name piece))
-                                           (format "@~a, ~a isn't taken"
-                                                   (piece->name piece)
-                                                   who)))))
-              (write participants)) (newline))
-           (`("?force" ,who)
-            (let* ((piece (random-piece))
-                   (result (add-participant who (random-piece))))
-              (match result
-                ('marbles-full
-                 (irc-send-message C
-                                   where
-                                   (format "@~a marbbies is full"
-                                           who)))
-                (_ (void)))))
-           ('("?pieces")
-            (irc-send-message C
-                              where
-                              (format "@~a ~a"
-                                      who
-                                      (string-join
-                                       (map piece->name (hash-keys participants))
-                                       ", "))))
-           ('("?commands")
-            (irc-send-message C
-                              where
-                              (format "@~a the commands are: ~a"
-                                      who
-                                      (string-join
-                                       commands
-                                       ", "))))
-           ('("?pieces-free")
-            (irc-send-message C
-                              where
-                              (format "@~a ~a"
-                                      who
-                                      (string-join
-                                       (map piece->name (available-pieces))
-                                       ", "))))
-           ('("?reset")
-            (when (is-moderator? message)
+(define (response-message message)
+  (match message
+    ((irc-message tags pref "PRIVMSG" `(,where ,what) message-whole)
+     (define who
+       (cdr (assq 'display-name (irc-message-tags message))))
+     (match (string-split what)
+       ('("?play")
+        ;; todo mutex or something 
+        (let* ((piece (random-piece))
+               (result (add-participant who (random-piece))))
+          (match result
+            ('ok
+             (format "@~a you have the ~a"
+                     who
+                     (piece->name piece)))
+            ('already-assigned
+             (format "@~a you already have a piece"
+                     who))
+            ('marbles-full
+             (format "@~a marbbies is full"
+                     who)))))
+       ('("?leave")
+        (remove-participant who)
+        (format "@~a left the game"
+                who))
+       ('("?what")
+        (let ((piece (participant-piece who)))
+          (if piece
+              (format "@~a you have the ~a"
+                      who
+                      (and piece
+                           (piece->name piece)))
+              (format "@~a you are not in the irl marbbies"
+                      who))))
+       (`("?who" ,piece)
+        (let* ((piece (string->symbol piece))
+               (pig (lookup-piece piece)))
+          (cond ((not (member piece pieces))
+                 (format "@~a, ~a is not a piece. expecting one of: ~a"
+                         who
+                         piece
+                         (string-join
+                          (map symbol->string pieces)
+                          ", ")))
+                (else
+                 (if pig
+                     (format "@~a, ~a has the ~a"
+                             who
+                             pig
+                             (piece->name piece))
+                     (format "@~a, ~a isn't taken"
+                             (piece->name piece)
+                             who))))))
+       (`("?force" ,who)
+        (let* ((piece (random-piece))
+               (result (add-participant who (random-piece))))
+          (match result
+            ('marbles-full
+             (format "@~a marbbies is full"
+                     who))
+            (_ (void)))))
+       ('("?pieces")
+        (format "@~a ~a"
+                who
+                (string-join
+                 (map piece->name (hash-keys participants))
+                 ", ")))
+       ('("?commands")
+        (format "@~a the commands are: ~a"
+                who
+                (string-join
+                 commands
+                 ", ")))
+       ('("?pieces-free")
+        (format "@~a ~a"
+                who
+                (string-join
+                 (map piece->name (available-pieces))
+                 ", ")))
+       ('("?reset")
+        (when (is-moderator? message)
               ;; todo improve conditions
-              (reset-marbles)
-              (irc-send-message C
-                                where
-                                (format "irl marbles has been reset by @~a"
-                                        who))))
-           (_
-            (write (list where what)) (newline)
-            (write message) (newline))))))
+          (reset-marbles)
+          (format "irl marbles has been reset by @~a"
+                  who)))
+       (_ #f))) ;; unrecognized command/not applicable
+    (_ #f))) ;; other kinds of messages
+
+(define (respond-to-message message)
+  (match message
+    ((irc-message _ _ "PRIVMSG" `(,where ,what)  _)
+     (define response
+       (response-message message))
+     (irc-send-message C where response))
+    (_ (void))))
 
 (define (gogo)
   (let loop ()
-    (define message (async-channel-get (irc-connection-incoming C)))
-    (match message
-      ((irc-message _ _ "PRIVMSG" `(,where ,what)  _)
-       (thread
-        (lambda ()
-          (respond-to-message message where what))))
-      (_
-       (write message)
-       (newline)))
+    (define message
+      (async-channel-get (irc-connection-incoming C)))
+    (thread
+     (lambda ()
+       (respond-to-message message)))
     (loop)))
 
 (define (main)
